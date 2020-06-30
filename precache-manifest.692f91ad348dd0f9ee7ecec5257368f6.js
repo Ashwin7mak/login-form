892901be66bdb31924b30bbd338edf7c1064ddba
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-06-29 19:30:10 -0700

    Updates

---
+++ precache-manifest.692f91ad348dd0f9ee7ecec5257368f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e34b46490a6e1644e5a17b59ebf618",
+    "url": "/login-form/index.html"
+  },
+  {
+    "revision": "aad9b6bf4873746cb3f2",
+    "url": "/login-form/static/css/main.5b64b5f1.chunk.css"
+  },
+  {
+    "revision": "6c2464e5c39b4f8e255d",
+    "url": "/login-form/static/js/2.2d78495a.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/login-form/static/js/2.2d78495a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad9b6bf4873746cb3f2",
+    "url": "/login-form/static/js/main.f2b56713.chunk.js"
+  },
+  {
+    "revision": "c698525b8492e632f731",
+    "url": "/login-form/static/js/runtime-main.af6c0941.js"
+  }
+]);
